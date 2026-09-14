6526f46172597cb46b103226594ca63f3d4a305a
khanfaiz16 <khanfaiztech1609@gmail.com>
2026-09-14 13:31:24 +0530

    js changes

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import { siteConfig, absoluteUrl } from '../data/seoConfig';
+import { siteConfig, absoluteUrl } from '../data/seoConfig.js';
 
 const MANAGED_NAME_TAGS = [
   'description',
